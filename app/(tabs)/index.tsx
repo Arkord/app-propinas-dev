45e97f1940b35d56e757a80679f3c1c7635d9c9a
octavio mejia <arkord1@gmail.com>
2024-09-09 00:52:37 -0500

    styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,116 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  SafeAreaView,
+  Dimensions,
+  StyleSheet,
+  Image,
+  ScrollView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+import React from "react";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import Svg, { Path } from "react-native-svg";
 
-export default function HomeScreen() {
+export default function Home() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView style={styles.container}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <View>
+          <View style={styles.box}>
+            <Svg
+              height={200}
+              width={Dimensions.get("screen").width}
+              viewBox="0 40 1440 320"
+            >
+              <Path
+                fill="#00cba9"
+                d="M0,224L60,234.7C120,245,240,267,360,229.3C480,192,600,96,720,96C840,96,960,192,1080,234.7C1200,277,1320,267,1380,261.3L1440,256L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
+              />
+            </Svg>
+          </View>
+
+          <View style={styles.header}>
+            <Image
+              source={require("../../assets/images/hat.png")}
+              style={styles.imageHeader}
+            />
+            <Text style={styles.textHeader}>TIPSr</Text>
+          </View>
+
+          <View style={styles.textBoxContainer}>
+            <View>
+              <TextInput
+                style={styles.textBox}
+                placeholder="Monto"
+                keyboardType="numeric"
+                underlineColorAndroid="transparent"
+                placeholderTextColor="#00cba9"
+              ></TextInput>
+            </View>
+
+            <View>
+            <TextInput
+                style={styles.textBox}
+                placeholder="Porcentaje"
+                keyboardType="numeric"
+                underlineColorAndroid="transparent"
+                placeholderTextColor="#00cba9"
+              ></TextInput>
+            </View>
+
+            <TouchableOpacity
+              onPress={() => {
+                Keyboard.dismiss();
+              }}
+            >
+              <Text>Calcular</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  header: {
+    marginTop: -40,
+    justifyContent: "center",
+    alignItems: "center",
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  textHeader: {
+    marginTop: -5,
+    textAlign: "center",
+    fontSize: 22,
+    textShadowColor: "rgba(0, 0, 0, 0.5)",
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 5,
+    color: "#000000",
   },
+  imageHeader: {
+    width: 40,
+    height: 40,
+  },
+  box: {
+    backgroundColor: "#00cba9",
+    height: 40,
+  },
+  textBoxContainer: {
+    marginTop: 60,
+  },
+  textBox: {
+    fontSize: 40,
+    color: "#00cba9",
+    borderColor: "#ccc",
+    borderBottomWidth: 3,
+    marginHorizontal: 20,
+  },
+  textBoxContent: {},
 });
